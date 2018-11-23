61d97a04d9b60164c0c4702ca82d1d899ed3ef2c
Leonid <leonu2007@yahoo.com>
2018-11-23 21:32:18 +0300

    #00000 img resize

--- js/app.js
+++ js/app.js
@@ -5,7 +5,14 @@ function handleFileSelect(evt) {
     if (file) {
         var reader = new FileReader();
         reader.onload = function(e) {
-            document.getElementById('preview').src = e.target.result;
+            var img = document.createElement("img");
+            img.src = e.target.result;
+            var canvas = resizeImage(img, file);
+
+            console.log(canvas);
+
+            document.getElementById('preview').src = canvas;
+
         };
         reader.readAsDataURL(file);
     }
@@ -13,3 +20,34 @@ function handleFileSelect(evt) {
 }
 
 document.getElementById('myFile').addEventListener('change', handleFileSelect, false);
+
+function resizeImage(img, file) {
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var MAX_WIDTH = 400;
+    var MAX_HEIGHT = 400;
+    var width = img.width;
+    var height = img.height;
+
+    if (width > height) {
+        if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+        }
+    } else {
+        if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+        }
+    }
+    canvas.width = width;
+    canvas.height = height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, width, height);
+
+    dataurl = canvas.toDataURL(file.type);
+
+    return dataurl;
+}
