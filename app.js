f7a9fba9153f7dbc4cd58b79a53313bde3d142bb
Leonid <leonu2007@yahoo.com>
2018-11-23 21:14:42 +0300

    #00000 img view

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 function handleFileSelect(evt) {
     let file = evt.target.files[0];
     console.log(file.name);
+
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('preview').src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+
 }
 
 document.getElementById('myFile').addEventListener('change', handleFileSelect, false);
