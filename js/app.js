12b45d8591f89f4cde6bae942a8ba3a9730edac6
Petr Kurapov <petr.a.kurapov@intel.com>
2018-11-24 21:01:28 +0300

    Fixed bootstrap, added copy to clip

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let gCanvas = undefined;
-let step = 4;
+let vstep = 4;
+let hstep = 4;
 
 symbols = ['\u28ff', '\u283f', '\u286a', '\u2805', '\u2802'];
 
@@ -39,7 +40,18 @@ function handleFileSelect(evt) {
 
             img.onload = function () {
                 console.log("LOADED");
-                gCanvas = resizeImage(this, file);
+
+                gCanvas = document.createElement("canvas");
+                let ctx = gCanvas.getContext("2d");
+
+                gCanvas.width = this.width;
+                gCanvas.height = this.height;
+
+                ctx.drawImage(this, 0, 0, this.width, this.height);
+                console.log(gCanvas.width, gCanvas.height);
+                console.log(this.width, this.height);
+
+                // gCanvas = resizeImage(this, file);
 
                 document.getElementById('preview').src = gCanvas.toDataURL(file.type);
             };
@@ -50,25 +62,35 @@ function handleFileSelect(evt) {
 
 }
 
+window.onload = function() {
+    document.getElementById('myFile').addEventListener('change', handleFileSelect, false);
+};
 
-document.getElementById('myFile').addEventListener('change', handleFileSelect, false);
 
 
 function processRequest() {
     let imgData = getCurrentPictureData();
     //makeItGray(imgData);
 
-    let res = prepareText(imgData);
-    // let matr = getMatrixFromImageData(imgData);
+    let canvas = resizeImage(document.getElementById('preview'));
+    let newImageData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+    // makeItBlackAndWhite(newImageData);
+    makeItGray(newImageData);
 
-    let text = generateTextImage(res);
+    let text = prepareText(newImageData);
+
+    text = generateTextFromMatrix(text);
     printResultToTextArea(text);
+    // printResultToTextArea(generateTextImage(text));
+
 
     // Just print stuff for fun
     makeItBlackAndWhite(imgData);
+
     gCanvas.getContext("2d").putImageData(imgData, 0, 0);
 
-    document.getElementById('preview2').src = gCanvas.toDataURL();
+    // document.getElementById('preview2').src = gCanvas.toDataURL();
+    // document.getElementById('preview3').src = canvas.toDataURL();
     // document.getElementById('output').innerHTML = text
 
 
@@ -92,22 +114,73 @@ function drawTheResult(asciiText) {
     alert(asciiText)
 }
 
+let base = '\u2800'.charCodeAt(0);
+console.log(base);
+console.log("base sign is ", String.fromCharCode(base + 1));
+console.log("base sign is ", String.fromCharCode(base + 0xff));
+
+function generateTextFromMatrix(matrix) {
+    let height = matrix.length - matrix.length % 4;
+    let width = matrix[0].length;
+
+    console.log(height, width);
+    let res = new Array(height/4).fill(0).map(() => new Array(width/2).fill(0));
+    let numbers = new Array(height/4).fill(0).map(() => new Array(width/2).fill(0));
+
+    let result = [];
+
+    console.log(res.length, res[0].length);
+    for (let i = 0, ii = 0; i < height; i += 4, ii += 1) {
+        let tmp = [];
+        for (let j = 0, jj = 0; j < width; j += 2, jj += 1) {
+            let binary = 0;
+            binary += matrix[i + 3][j + 1];
+            binary <<= 1;
+            binary += matrix[i + 3][j];
+            binary <<= 1;
+            binary += matrix[i + 2][j + 1];
+            binary <<= 1;
+            binary += matrix[i + 1][j + 1];
+            binary <<= 1;
+            binary += matrix[i][j + 1];
+            binary <<= 1;
+            binary += matrix[i + 2][j];
+            binary <<= 1;
+            binary += matrix[i + 1][j];
+            binary <<= 1;
+            binary += matrix[i][j];
+
+            if (binary === 0)
+                binary = 1;
+
+            let sign = String.fromCharCode(base + binary);
+            tmp.push(sign);
+            res[ii][jj] = sign;
+            numbers[ii][jj] = binary
+        }
+        tmp.push('\n');
+        result.push(tmp.join(""));
+    }
+
+    console.log(numbers);
+    return result.join("")
+}
 
 function generateTextImage(dataArray) {
-    let text = [];
     let mult = [];
     for (let i = 0; i < dataArray.length; i += 1) {
         let tmp = [];
         for (let j = 0; j < dataArray[i].length; j += 1) {
-            text.push(symbols[dataArray[i][j]]);
+
             tmp.push(symbols[dataArray[i][j]]);
         }
-        mult.push(tmp)
+        tmp.push('\n');
+        mult.push(tmp.join(""));
     }
 
     console.log(mult);
-    console.log(text);
-    return text.join("")
+    // return text.join("")
+    return mult.join("");
 }
 
 
@@ -130,35 +203,32 @@ function printResultToTextArea(str) {
     document.getElementById("pictext").value = str
 }
 
-
-function superTextTransform(imageData) {
-
-}
-
 function prepareText(imageData) {
 
     //console.log(imageData.data[0] + "+++++++++" + imageData.data[1] + "+++++++++" + imageData.data[2] + "+++++++++" + imageData.data[3]);
 
-    let max = 3 * step * step * 255;
-    let part = max / 4;
+    let max = 3 * vstep * hstep * 255;
+    let threshold = max / 1.9;
     let result = [];
 
     let row = 0;
 
-    for (let i = 0; i < imageData.height; i += step) {
+    //console.log("height = " + (imageData.height - imageData.height % 4));
+
+    for (let i = 0; i < imageData.height; i += vstep) {
         let line = [];
         let column = 0;
-        for(let m = 0; m < imageData.width * 4; m += step * 4) {
+        for(let m = 0; m < imageData.width * 4; m += hstep * 4) {
             let sum = 0;
-            for (let j = 0; j < step; j++) {
-                for (let k = 0; k < step * 4; k += 4) {
+            for (let j = 0; j < vstep; j++) {
+                for (let k = 0; k < hstep * 4; k += 4) {
                     for (let p = 0; p < 3; p++) {
                         //console.log(i * imageData.width + m + j * imageData.width * 4 + k + p);
                         sum += imageData.data[i * imageData.width * 4 + m + j * imageData.width * 4 + k + p];
                     }
                 }
             }
-            line[column] = Math.floor(sum / part);
+            line[column] = sum  > threshold ? 0 : 1;
             column++;
             // console.log(sum);
         }
@@ -198,7 +268,7 @@ function makeItBlackAndWhite(imageData) {
 }
 
 
-function resizeImage(img, file) {
+function resizeImage(img) {
     var canvas = document.createElement("canvas");
     var ctx = canvas.getContext("2d");
     ctx.drawImage(img, 0, 0);
@@ -225,4 +295,13 @@ function resizeImage(img, file) {
     console.log(img.width, img.height);
 
     return canvas;
+}
+
+
+function copyToClip() {
+    let copyText = document.getElementById("pictext");
+    copyText.select();
+    document.execCommand("copy");
+
+    // todo: turn button text to copied! for 3 sec
 }
